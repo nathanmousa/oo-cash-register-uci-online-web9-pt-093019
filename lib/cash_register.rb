c0a654c9fb46f6239cd2abd941b2e9cfd69d4fa5
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-30 02:21:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,7 +19,7 @@ class CashRegister
   
   def apply_discount
     if @discount > 0
-      discount = @discount / 100
-      @total = @total - (@total * discount)
+      discount = 1.00 - (discount / 100)
+      total *= total - (total * discount)
   end
 end
